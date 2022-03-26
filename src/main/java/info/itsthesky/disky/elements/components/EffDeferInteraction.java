8dca86b0e92d8e4cea5a39ef4f9ad95fd26ab0dc
wallace4BR <wallaceinacio02@gmail.com>
2022-03-25 21:05:33 -0300

    🚀 finish interaction stuff

--- src/main/java/info/itsthesky/disky/elements/components/EffDeferInteraction.java
+++ src/main/java/info/itsthesky/disky/elements/components/EffDeferInteraction.java
@@ -20,42 +20,19 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Defer Interaction")
-@Description({"Only usable in interaction event, currently button click & dropdown update event!",
-"\nThis will force the interaction to be acknowledge, sending a success message to Discord.",
+@Description({"Only usable in interaction event, currently button click/dropdown update/modal event!",
+"\nThis will force the interaction to be acknowledge, you have 3 seconds to do so, the effect will send a success message to Discord or hold the interaction to send a message later.",
 "\nKeep in mind that replying in an interaction event will automatically defer the interaction, and therefore you don't need to defer it.",
+"\nIf you need to wait more than 3 seconds use the and wait pattern",
 "\nAn interaction can only be deferred once!"})
-@Examples("defer the interaction")
+@Examples("defer the interaction [and wait]")
 public class EffDeferInteraction extends Effect {
 
     static {
         Skript.registerEffect(
                 EffDeferInteraction.class,
-                "(acknowledge|defer) [the] interaction"
+                "(acknowledge|defer) [the] interaction [and wait (1¦silently)]"
         );
-    }
-
-
-    @Override
-    protected void execute(@NotNull Event e) {
-        GenericInteractionCreateEvent event = ((InteractionEvent) e).getInteractionEvent();
-
-        if (event instanceof GenericComponentInteractionCreateEvent) {
-
-            GenericComponentInteractionCreateEvent clickEvent = (GenericComponentInteractionCreateEvent) event;
-            clickEvent.deferEdit().queue(null, ex -> DiSky.getErrorHandler().exception(e, ex));
-
-        } else if (event instanceof ModalInteractionEvent) {
-
-            ModalInteractionEvent clickEvent = (ModalInteractionEvent) event;
-            clickEvent.deferEdit().queue(null, ex -> DiSky.getErrorHandler().exception(e, ex));
-
-        }
-
-    }
-
-    @Override
-    public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "defer the interaction";
     }
 
     private NodeInformation node;
@@ -70,6 +47,40 @@ public class EffDeferInteraction extends Effect {
 
         node = new NodeInformation();
 
+        isEphemeral = parseResult.mark == 1;
+        shouldwait = parseResult.expr.contains("and wait");
+
         return true;
     }
+
+    private boolean isEphemeral;
+    private boolean shouldwait;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        GenericInteractionCreateEvent event = ((InteractionEvent) e).getInteractionEvent();
+
+        if (event instanceof GenericComponentInteractionCreateEvent) {
+
+            GenericComponentInteractionCreateEvent clickEvent = (GenericComponentInteractionCreateEvent) event;
+
+        } else if (event instanceof ModalInteractionEvent) {
+
+            ModalInteractionEvent clickEvent = (ModalInteractionEvent) event;
+
+        if (shouldwait) {
+            clickEvent.deferReply(isEphemeral).queue(null, ex -> DiSky.getErrorHandler().exception(e, ex));
+                return;
+            }
+            clickEvent.deferEdit().queue(null, ex -> DiSky.getErrorHandler().exception(e, ex));
+
+        }
+
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "defer the interaction";
+    }
+
 }
--- src/main/java/info/itsthesky/disky/elements/effects/ReplyWith.java
+++ src/main/java/info/itsthesky/disky/elements/effects/ReplyWith.java
@@ -18,6 +18,7 @@ import info.itsthesky.disky.elements.components.core.ComponentRow;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
@@ -74,6 +75,7 @@ public class ReplyWith extends SpecificBotEffect<Message> {
 		if (isInInteraction) {
 
 			final IReplyCallback event = (IReplyCallback) ((InteractionEvent) e).getInteractionEvent();
+			final InteractionHook hook = event.getHook();
 			final Object rawMessage = parseSingle(exprMessage, e, null);
 			final MessageBuilder message = JDAUtils.constructMessage(rawMessage);
 			final List<ComponentRow> rows = Arrays.asList(parseList(exprComponents, e, new ComponentRow[0]));
@@ -86,7 +88,13 @@ public class ReplyWith extends SpecificBotEffect<Message> {
 					.stream()
 					.map(ComponentRow::asActionRow)
 					.collect(Collectors.toList());
-
+			if (!hook.isExpired()) {
+				hook.editOriginal(message.build())
+				.queue(v -> restart(), ex -> {
+					DiSky.getErrorHandler().exception(e, ex);
+					restart();
+				});
+			}
 			event.reply(message.build())
 					.addActionRows(formatted)
 					.setEphemeral(hidden)
