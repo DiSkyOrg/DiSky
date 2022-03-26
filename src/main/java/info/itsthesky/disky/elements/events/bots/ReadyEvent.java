bf387d72b699751f7dc7335ee88a17dccd02e411
Sky <livegogo.16334@gmail.com>
2022-03-26 13:43:25 +0100

    :ambulance: Fixed 'on ready' & 'on guild ready' events that were not fired/parsed

---
+++ src/main/java/info/itsthesky/disky/elements/events/bots/ReadyEvent.java
@@ -0,0 +1,23 @@
+package info.itsthesky.disky.elements.events.bots;
+
+import info.itsthesky.disky.api.events.DiSkyEvent;
+import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.core.SkriptUtils;
+
+public class ReadyEvent extends DiSkyEvent<net.dv8tion.jda.api.events.ReadyEvent> {
+
+	static {
+		register("Ready Event", ReadyEvent.class, BukkitReadyEvent.class,
+				"(ready|bot load[ed])")
+				.description("Fired when a bot is fully loaded. 'guild ready' should be called before this one.");
+
+		SkriptUtils.registerBotValue(BukkitReadyEvent.class);
+	}
+	
+	public static class BukkitReadyEvent extends SimpleDiSkyEvent<net.dv8tion.jda.api.events.ReadyEvent> {
+		public BukkitReadyEvent(ReadyEvent event) {
+			
+		}
+	}
+	
+}
