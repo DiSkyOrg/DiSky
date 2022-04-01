62eb206f24e98808b8602baa53651d40fcbb4641
Sky <livegogo.16334@gmail.com>
2022-04-01 18:54:33 +0200

    :sparkles: Added bot join event

---
+++ src/main/java/info/itsthesky/disky/elements/events/bots/BotJoinEvent.java
@@ -0,0 +1,31 @@
+package info.itsthesky.disky.elements.events.bots;
+
+import info.itsthesky.disky.api.events.DiSkyEvent;
+import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.core.SkriptUtils;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+
+public class BotJoinEvent extends DiSkyEvent<GuildJoinEvent> {
+
+	static {
+		register("Bot Join Event", BotJoinEvent.class, BukkitBotJoinEvent.class,
+				"bot [guild] join[ed]")
+				.description("Fired when any bot join a new guild.");
+
+		SkriptUtils.registerBotValue(BukkitBotJoinEvent.class);
+
+		SkriptUtils.registerValue(BukkitBotJoinEvent.class, Guild.class, BotJoinEvent::apply);
+	}
+
+	private static Guild apply(BukkitBotJoinEvent e) {
+		return e.getJDAEvent().getGuild();
+	}
+
+	public static class BukkitBotJoinEvent extends SimpleDiSkyEvent<GuildJoinEvent> {
+		public BukkitBotJoinEvent(BotJoinEvent event) {
+			
+		}
+	}
+	
+}
