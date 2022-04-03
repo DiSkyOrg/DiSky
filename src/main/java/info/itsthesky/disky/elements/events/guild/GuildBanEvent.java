1c154d15d5c25b0abee360030d0aa6d8ca660fef
wallace4BR <81590228+wallace4BR@users.noreply.github.com>
2022-04-03 10:54:22 -0300

    Some additions (#18)
    * :rocket: add guild ban event
    
    * :rocket: refactor guild unban event
    
    * :rocket: make destroy entity capable of destroy emotes

---
+++ src/main/java/info/itsthesky/disky/elements/events/guild/GuildBanEvent.java
@@ -0,0 +1,33 @@
+package info.itsthesky.disky.elements.events.guild;
+
+import info.itsthesky.disky.api.events.DiSkyEvent;
+import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.core.SkriptUtils;
+import net.dv8tion.jda.api.entities.*;
+
+public class GuildBanEvent extends DiSkyEvent<net.dv8tion.jda.api.events.guild.GuildBanEvent> {
+
+    static {
+        register("Guild Unban Event", GuildBanEvent.class, BukkitGuildBanEvent.class,
+                "[discord] guild [user] ban)")
+                .description("Fired when a user is banned from a guild. \n A member doesn't exist here because the member is not in the guild anymore! \n can be used to get the banned user, the author and the guild.")
+                .examples("on guild unban:");
+
+
+        SkriptUtils.registerBotValue(GuildBanEvent.BukkitGuildBanEvent.class);
+
+        SkriptUtils.registerAuthorValue(GuildBanEvent.BukkitGuildBanEvent.class, e -> e.getJDAEvent().getGuild());
+
+        SkriptUtils.registerValue(GuildBanEvent.BukkitGuildBanEvent.class, User.class,
+                event -> event.getJDAEvent().getUser(), 0);
+
+        SkriptUtils.registerValue(GuildBanEvent.BukkitGuildBanEvent.class, Guild.class,
+                event -> event.getJDAEvent().getGuild(), 0);
+
+    }
+
+    public static class BukkitGuildBanEvent extends SimpleDiSkyEvent<net.dv8tion.jda.api.events.guild.GuildBanEvent> {
+        public BukkitGuildBanEvent(GuildBanEvent event) {
+        }
+    }
+}
--- src/main/java/info/itsthesky/disky/elements/events/guild/GuildUnbanEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/guild/GuildUnbanEvent.java
@@ -8,26 +8,26 @@ import net.dv8tion.jda.api.entities.*;
 public class GuildUnbanEvent extends DiSkyEvent<net.dv8tion.jda.api.events.guild.GuildUnbanEvent> {
 
     static {
-        register("Guild Unban Event", GuildUnbanEvent.class, BukkitUserUnbanEvent.class,
+        register("Guild Unban Event", GuildUnbanEvent.class, BukkitGuildUnbanEvent.class,
                 "[discord] guild [user] unban)")
                 .description("Fired when a user is unbanned \n can be used to get the unbanned user, the author and the guild.")
                 .examples("on guild unban:");
 
 
-        SkriptUtils.registerBotValue(GuildUnbanEvent.BukkitUserUnbanEvent.class);
+        SkriptUtils.registerBotValue(GuildUnbanEvent.BukkitGuildUnbanEvent.class);
 
-        SkriptUtils.registerAuthorValue(GuildUnbanEvent.BukkitUserUnbanEvent.class, e -> e.getJDAEvent().getGuild());
+        SkriptUtils.registerAuthorValue(GuildUnbanEvent.BukkitGuildUnbanEvent.class, e -> e.getJDAEvent().getGuild());
 
-        SkriptUtils.registerValue(GuildUnbanEvent.BukkitUserUnbanEvent.class, User.class,
+        SkriptUtils.registerValue(GuildUnbanEvent.BukkitGuildUnbanEvent.class, User.class,
                 event -> event.getJDAEvent().getUser(), 0);
 
-        SkriptUtils.registerValue(GuildUnbanEvent.BukkitUserUnbanEvent.class, Guild.class,
+        SkriptUtils.registerValue(GuildUnbanEvent.BukkitGuildUnbanEvent.class, Guild.class,
                 event -> event.getJDAEvent().getGuild(), 0);
 
     }
 
-    public static class BukkitUserUnbanEvent extends SimpleDiSkyEvent<net.dv8tion.jda.api.events.guild.GuildUnbanEvent> {
-        public BukkitUserUnbanEvent(GuildUnbanEvent event) {
+    public static class BukkitGuildUnbanEvent extends SimpleDiSkyEvent<net.dv8tion.jda.api.events.guild.GuildUnbanEvent> {
+        public BukkitGuildUnbanEvent(GuildUnbanEvent event) {
         }
     }
 }
