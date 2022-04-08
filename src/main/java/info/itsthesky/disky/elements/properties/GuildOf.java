fa6eebcf85013e0790759d7b74851a86fef03901
Sky <livegogo.16334@gmail.com>
2022-04-08 21:39:02 +0200

    :sparkles: Added guild of expression

---
+++ src/main/java/info/itsthesky/disky/elements/properties/GuildOf.java
@@ -0,0 +1,47 @@
+package info.itsthesky.disky.elements.properties;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Guild Of")
+@Description({"Return the guild of a specific entity.",
+"This can return null if the entity is not guild-based, like private message channel or message."})
+@Examples({"guild of event-member",
+"guild of event-channel"})
+public class GuildOf extends SimplePropertyExpression<Object, Guild> {
+
+    static {
+        register(
+                GuildOf.class,
+                Guild.class,
+                "",
+                "channel/role/member/message"
+        );
+    }
+
+    @Override
+    protected @NotNull String getPropertyName() {
+        return "guild";
+    }
+
+    @Override
+    public @Nullable Guild convert(Object entity) {
+        if (entity instanceof Role)
+            return ((Role) entity).getGuild();
+        if (entity instanceof BaseGuildMessageChannel)
+            return ((BaseGuildMessageChannel) entity).getGuild();
+        if (entity instanceof Member)
+            return ((Member) entity).getGuild();
+        return null;
+    }
+
+    @Override
+    public @NotNull Class<? extends Guild> getReturnType() {
+        return Guild.class;
+    }
+}
