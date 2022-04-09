ef8c90e023ce9c7aab1349f567106ffc168c4250
DiSky Master <101949465+DiSkyMaster@users.noreply.github.com>
2022-04-10 00:23:28 +0300

    Update GuildBoostCount.java

--- src/main/java/info/itsthesky/disky/elements/properties/guilds/GuildBoostCount.java
+++ src/main/java/info/itsthesky/disky/elements/properties/guilds/GuildBoostCount.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.entities.Guild;
 
 @Name("Guild Booster Count")
 @Description({"Represent how many people are boosting the guild currently."})
-@Examples("reply with \"There's %boost count of event-guild% booster(s)!\"")
+@Examples("reply with \"There's %booster amount of event-guild% booster(s)!\"")
 public class GuildBoostCount extends GuildProperty<Number>  {
 
     static {
