36fc6e42110daff83c11b10c29429d911ee5094e
DiSky Master <101949465+DiSkyMaster@users.noreply.github.com>
2022-04-10 00:07:43 +0300

    Update MemberRoleRemoveEvent.java

--- src/main/java/info/itsthesky/disky/elements/events/member/MemberRoleRemoveEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/member/MemberRoleRemoveEvent.java
@@ -11,8 +11,7 @@ public class MemberRoleRemoveEvent extends DiSkyEvent<GuildMemberRoleRemoveEvent
     static {
         register("Role Remove Event", MemberRoleAddEvent.class, BukkitMemberRemoveEvent.class,
                 "[discord] [member] role remove")
-                .description("Fired when a member remove roles from another member, it's a log action so event-author returns who made the action" +
-                        "event-roles returns a list of removed roles")
+                .description("Fired when a member remove roles from another member, it's a log action so event-author returns who made the action, event-roles returns a list of removed roles")
                 .examples("on role remove:");
 
 
