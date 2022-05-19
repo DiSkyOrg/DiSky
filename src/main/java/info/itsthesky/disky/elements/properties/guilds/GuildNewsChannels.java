9bf5f93d0a7006bc8e3819f5b5e4bcdc38fb2fa4
Sky <livegogo.16334@gmail.com>
2022-05-19 17:03:09 +0200

    :sparkles: Added voice, text, guild, stage and news channels of guild

---
+++ src/main/java/info/itsthesky/disky/elements/properties/guilds/GuildNewsChannels.java
@@ -0,0 +1,25 @@
+package info.itsthesky.disky.elements.properties.guilds;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import jdk.jfr.Name;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.NewsChannel;
+
+@Name("Guild News Channels")
+@Description("Gets all news channels of a guild.")
+@Examples("all news channels of event-guild")
+public class GuildNewsChannels extends MultipleGuildProperty<NewsChannel> {
+
+    static {
+        register(GuildNewsChannels.class,
+                NewsChannel.class,
+                "[all] news[( |-)]channels");
+    }
+
+    @Override
+    public NewsChannel[] converting(Guild guild) {
+        return guild.getNewsChannels().toArray(new NewsChannel[0]);
+    }
+
+}
