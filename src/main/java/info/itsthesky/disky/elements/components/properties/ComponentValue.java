dc1816c116ca2ecc4e56aa8613c3d1e645989008
Sky <livegogo.16334@gmail.com>
2022-06-25 14:42:07 +0200

    :sparkles: Reworked modal component value expression to work with dropdown

--- src/main/java/info/itsthesky/disky/elements/components/properties/ComponentValue.java
+++ src/main/java/info/itsthesky/disky/elements/components/properties/ComponentValue.java
@@ -11,44 +11,53 @@ import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import info.itsthesky.disky.api.skript.EasyElement;
 import info.itsthesky.disky.elements.events.interactions.ModalSendEvent;
+import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Name("Component Value")
-@Description("Get the current value of a sent component, currently only working in modals with text input.")
-@Examples("value of component with id \"XXX\"")
-public class ComponentValue extends SimpleExpression<String> {
+@Name("Modal Component Value / Values")
+@Description({"Get the current value(s) of a sent component, currently only working in modals with text input & select menus.",
+		"You have to precise what type of component you are trying to get, either 'textinput' or 'dropdown'."})
+@Examples({"values of dropdown with id \"XXX\"",
+		"value of textinput with id \"XXX\""})
+public class ComponentValue extends SimpleExpression<Object> {
 
 	static {
 		Skript.registerExpression(
 				ComponentValue.class,
-				String.class,
+				Object.class,
 				ExpressionType.COMBINED,
-				"[the] [current] value of [the] (component|text[( |-)]input) [with [the] id] %string%"
+				"[the] [current] value[s] of [the] (1¦text[( |-)]input|2¦drop[( |-)]down) [with [the] id] %string%"
 		);
 	}
 
 	private Expression<String> exprId;
 
 	@Override
-	protected String @NotNull [] get(@NotNull Event e) {
+	protected Object @NotNull [] get(@NotNull Event e) {
 		final String id = EasyElement.parseSingle(exprId, e, null);
 		if (EasyElement.anyNull(id))
 			return new String[0];
-		return new String[] {
-				((ModalSendEvent.BukkitModalSendEvent) e).getJDAEvent().getValue(id).getAsString()
-		};
+		final ModalMapping mapping = ((ModalSendEvent.BukkitModalSendEvent) e).getJDAEvent().getValue(id);
+		if (mapping == null)
+			return new String[0];
+
+		if (isSingle())
+			return new String[] { mapping.getAsString() };
+		else
+			return mapping.getAsStringList().toArray(new String[0]);
+
 	}
 
 	@Override
 	public boolean isSingle() {
-		return true;
+		return isSingle;
 	}
 
 	@Override
-	public @NotNull Class<? extends String> getReturnType() {
-		return String.class;
+	public @NotNull Class<?> getReturnType() {
+		return returnType;
 	}
 
 	@Override
@@ -56,12 +65,17 @@ public class ComponentValue extends SimpleExpression<String> {
 		return "value of component with id " + exprId.toString(e, debug);
 	}
 
+	private Class<?> returnType;
+	private boolean isSingle;
+
 	@Override
 	public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
 		if (!EasyElement.containsEvent(ModalSendEvent.BukkitModalSendEvent.class)) {
 			Skript.error("You can only get values of components in a modal receive event.");
 			return false;
 		}
+		returnType = parseResult.mark == 1 ? String.class : String[].class;
+		isSingle = parseResult.mark == 1;
 		exprId = (Expression<String>) exprs[0];
 		return true;
 	}
