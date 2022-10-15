04ebbc562b84343de9c3b65be39cba3fc07ddb5d
Sky <livegogo.16334@gmail.com>
2022-10-15 12:42:51 +0200

    :zap: Made 'unregister command' effect works with names

--- src/main/java/info/itsthesky/disky/elements/components/commands/UnregisterCommand.java
+++ src/main/java/info/itsthesky/disky/elements/components/commands/UnregisterCommand.java
@@ -1,6 +1,9 @@
 package info.itsthesky.disky.elements.components.commands;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
@@ -8,14 +11,21 @@ import info.itsthesky.disky.DiSky;
 import info.itsthesky.disky.api.skript.SpecificBotEffect;
 import info.itsthesky.disky.core.Bot;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.requests.RestAction;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Name("Unregister Command")
+@Description({"Unregister a specific slash command from local or global context of a bot.",
+"You must provide the command's name. Keep in mind this **SHOULD NOT** be used!",
+"The best way remains to update bot's commands without the command you want to delete!"})
+@Examples("unregister command \"test\" locally in guild with id \"000\"")
 public class UnregisterCommand extends SpecificBotEffect {
 
     static {
@@ -23,13 +33,13 @@ public class UnregisterCommand extends SpecificBotEffect {
                 "unregister [the] [command[s]] %strings% [(1¦globally|2¦locally)] (in|from|of) [the] [(bot|guild)] %bot/guild%");
     }
 
-    private Expression<String> exprIds;
+    private Expression<String> exprNames;
     private boolean isGlobal;
     private Expression<Object> exprEntity;
 
     @Override
     public boolean initEffect(Expression[] expressions, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
-        exprIds = (Expression<String>) expressions[0];
+        exprNames = (Expression<String>) expressions[0];
         exprEntity = (Expression<Object>) expressions[1];
         isGlobal = (parseResult.mark & 1) != 0;
         return true;
@@ -38,33 +48,35 @@ public class UnregisterCommand extends SpecificBotEffect {
 
     @Override
     public void runEffect(@NotNull Event e, Bot bot) {
-        final String[] ids = parseList(exprIds, e, new String[0]);
+        final String[] names = parseList(exprNames, e, new String[0]);
         final Object entity = parseSingle(exprEntity, e, null);
-        if (ids.length == 0 || entity == null) {
+        if (names.length == 0 || entity == null) {
             restart();
             return;
         }
 
         final @Nullable Guild guild = isGlobal ? null : (Guild) entity;
-        final RestAction<?> action;
 
-        if (isGlobal)
-            action = RestAction.allOf(Stream.of(ids)
-                    .map(id -> bot.getInstance().deleteCommandById(id))
-                    .collect(Collectors.toList()));
-        else
-            action = RestAction.allOf(Stream.of(ids)
-                    .map(guild::deleteCommandById)
+        final RestAction<List<Command>> restAction = isGlobal ? bot.getInstance().retrieveCommands() : guild.retrieveCommands();
+
+        restAction.queue(commands -> {
+            final List<Command> toDelete = commands.stream()
+                    .filter(command -> Stream.of(names).anyMatch(name -> name.equalsIgnoreCase(command.getName())))
+                    .collect(Collectors.toList());
+
+            final RestAction<?> action = RestAction.allOf(toDelete.stream()
+                    .map(Command::delete)
                     .collect(Collectors.toList()));
 
-        action.queue(this::restart, ex -> {
-            restart();
-            DiSky.getErrorHandler().exception(e, ex);
+            action.queue(this::restart, ex -> {
+                restart();
+                DiSky.getErrorHandler().exception(e, ex);
+            });
         });
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "unregister commands " + exprIds.toString(e, debug) + " in " + (isGlobal ? "global" : "local") + " in " + exprEntity.toString(e, debug);
+        return "unregister commands " + exprNames.toString(e, debug) + " in " + (isGlobal ? "global" : "local") + " in " + exprEntity.toString(e, debug);
     }
 }
