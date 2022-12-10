e27344f5825cc3fb996dbdeac76ec84e0cb10be8
Sky <livegogo.16334@gmail.com>
2022-12-10 18:22:29 +0100

    :sparkles: Added way to get scheduled event from its ID

---
+++ src/main/java/info/itsthesky/disky/elements/getters/GetScheduledEvent.java
@@ -0,0 +1,38 @@
+package info.itsthesky.disky.elements.getters;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import info.itsthesky.disky.core.Bot;
+import net.dv8tion.jda.api.entities.ScheduledEvent;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import org.jetbrains.annotations.NotNull;
+
+@Name("Get Scheduled Event")
+@Description({"Get a scheduled event from a guild using its unique ID.",
+        "Scheduled events are global on discord, means different scheduled events cannot have the same ID.",
+        "This expression cannot be changed."})
+@Examples("scheduled event with id \"000\"")
+public class GetScheduledEvent extends BaseGetterExpression<ScheduledEvent> {
+
+    static {
+        register(GetScheduledEvent.class,
+                ScheduledEvent.class,
+                "scheduled event");
+    }
+
+    @Override
+    protected ScheduledEvent get(String id, Bot bot) {
+        return bot.getInstance().getScheduledEventById(id);
+    }
+
+    @Override
+    public String getCodeName() {
+        return "scheduled event";
+    }
+
+    @Override
+    public @NotNull Class<? extends ScheduledEvent> getReturnType() {
+        return ScheduledEvent.class;
+    }
+}
