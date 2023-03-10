30e624887df0eceb8fa49a54b99fa642ef94e6a1
Sky <livegogo.16334@gmail.com>
2023-03-10 18:44:09 +0100

    :zap: Added documentation to log properties

--- src/main/java/info/itsthesky/disky/api/skript/ReflectClassFactory.java
+++ src/main/java/info/itsthesky/disky/api/skript/ReflectClassFactory.java
@@ -1,8 +1,13 @@
 package info.itsthesky.disky.api.skript;
 
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
@@ -43,18 +48,54 @@ public final class ReflectClassFactory {
 		}
 	}
 
+	public static class Documentation {
+
+		private final String name;
+		private final String[] description;
+		private final String[] examples;
+		private final String[] since;
+
+		public Documentation(String name, String description, String examples, String since) {
+			this.name = name;
+			this.description = description.split("\n");
+			this.examples = examples.split("\n");
+			this.since = since.split("\n");
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String[] getDescription() {
+			return description;
+		}
+
+		public String[] getExamples() {
+			return examples;
+		}
+
+		public String[] getSince() {
+			return since;
+		}
+	}
+
 	private static final AtomicInteger COUNT = new AtomicInteger();
-	public static <F, T> void register(Class<F> fromType,
-									   String fromTypeName,
+	public static <F, T> void register(String fromTypeName,
 									   String propertyName,
 									   Class<T> toType,
 									   String property,
-									   Function<F, T> converter) {
+									   Function<F, T> converter,
+									   Documentation documentation) {
 		try {
 
 			final Class<?> elementClass = new ByteBuddy()
 					.redefine(ReflectProperty.class)
 					.name("ReflectProperty_" + COUNT.incrementAndGet())
+
+					.annotateType(AnnotationDescription.Builder.ofType(Name.class).define("value", documentation.getName()).build())
+					.annotateType(AnnotationDescription.Builder.ofType(Description.class).defineArray("value", documentation.getDescription()).build())
+					.annotateType(AnnotationDescription.Builder.ofType(Examples.class).defineArray("value", documentation.getExamples()).build())
+					.annotateType(AnnotationDescription.Builder.ofType(Since.class).defineArray("value", documentation.getSince()).build())
 
 					.method(named("convert")).intercept(MethodDelegation.to(new ConvertMethodInterceptor<>(converter)))
 					.method(named("getPropertyName")).intercept(MethodDelegation.to(new PropertyNameMethodInterceptor(propertyName)))
--- src/main/java/info/itsthesky/disky/elements/properties/ConstLogs.java
+++ src/main/java/info/itsthesky/disky/elements/properties/ConstLogs.java
@@ -1,5 +1,6 @@
 package info.itsthesky.disky.elements.properties;
 
+import info.itsthesky.disky.api.skript.ReflectClassFactory;
 import net.dv8tion.jda.api.audit.ActionType;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
 import net.dv8tion.jda.api.entities.Guild;
@@ -11,10 +12,18 @@ public final class ConstLogs {
 
     public static void registerAll() {
 
-        register(AuditLogEntry.class, "logentry", "logged user", User.class, "log[ged] (user|author)", AuditLogEntry::getUser);
-        register(AuditLogEntry.class, "logentry", "logged guild", Guild.class, "log[ged] guild", AuditLogEntry::getGuild);
-        register(AuditLogEntry.class, "logentry", "logged id", String.class, "log[ged] id", AuditLogEntry::getId);
-        register(AuditLogEntry.class, "logentry", "logged action", ActionType.class, "log[ged] action [type]", AuditLogEntry::getType);
+        register( "logentry", "logged user", User.class, "log[ged] (user|author)", AuditLogEntry::getUser,
+                new ReflectClassFactory.Documentation("Logged User", "The user who triggered the log entry.",
+                        "logged user of event-logentry", "4.11.0"));
+        register( "logentry", "logged guild", Guild.class, "log[ged] guild", AuditLogEntry::getGuild,
+                new ReflectClassFactory.Documentation("Logged Guild", "The guild where the log entry has been triggered.",
+                        "logged guild of event-logentry", "4.11.0"));
+        register( "logentry", "logged id", String.class, "log[ged] id", AuditLogEntry::getId,
+                new ReflectClassFactory.Documentation("Logged ID", "The ID of the log entry.",
+                        "logged id of event-logentry", "4.11.0"));
+        register( "logentry", "logged action", ActionType.class, "log[ged] action [type]", AuditLogEntry::getType,
+                new ReflectClassFactory.Documentation("Logged Action", "The action type of the log entry.",
+                        "logged action of event-logentry", "4.11.0"));
 
     }
 }
