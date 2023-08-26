046b85f76a30038c4797ebb4b78dd324ce24c16c
Nicolas RACOT <livegogo.16334@gmail.com>
2023-08-26 23:23:44 +0200

    :sparkles: Added JSON serialization for embeds

---
+++ src/main/java/info/itsthesky/disky/elements/properties/embeds/EmbedToJSON.java
@@ -0,0 +1,58 @@
+package info.itsthesky.disky.elements.properties.embeds;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import info.itsthesky.disky.api.skript.EasyElement;
+import info.itsthesky.disky.core.Utils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class EmbedToJSON extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(
+                EmbedToJSON.class,
+                String.class,
+                ExpressionType.COMBINED,
+                "[the] embed %embedbuilder% (to|as) json"
+        );
+    }
+
+    private Expression<EmbedBuilder> exprJSON;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        exprJSON = (Expression<EmbedBuilder>) exprs[0];
+        return true;
+    }
+
+    @Override
+    protected String @NotNull [] get(@NotNull Event e) {
+        final EmbedBuilder embedBuilder = EasyElement.parseSingle(exprJSON, e);
+        if (embedBuilder == null)
+            return new String[0];
+
+        return new String[] {Utils.convertEmbedToJSON(embedBuilder)};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "embed from json " + exprJSON.toString(e, debug);
+    }
+}
