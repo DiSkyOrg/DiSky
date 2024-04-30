5a4565f1a3653f06d5f559336a067316943929c8
Sky <livegogo.16334@gmail.com>
2024-04-30 19:13:22 +0200

    :sparkles: Added `event-user` and `event-member` in Member Boost Event

--- src/main/java/info/itsthesky/disky/elements/events/member/MemberBoostEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/member/MemberBoostEvent.java
@@ -4,6 +4,8 @@ import info.itsthesky.disky.api.events.DiSkyEvent;
 import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
 import info.itsthesky.disky.core.SkriptUtils;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
 
 import java.time.OffsetDateTime;
@@ -18,8 +20,6 @@ public class MemberBoostEvent extends DiSkyEvent<GuildMemberUpdateBoostTimeEvent
 
         SkriptUtils.registerBotValue(MemberBoostEvent.BukkitMemberBoostEvent.class);
 
-        SkriptUtils.registerAuthorValue(MemberBoostEvent.BukkitMemberBoostEvent.class, e -> e.getJDAEvent().getGuild());
-
         SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, OffsetDateTime.class,
                 event -> event.getJDAEvent().getOldValue(), -1);
 
@@ -32,6 +32,11 @@ public class MemberBoostEvent extends DiSkyEvent<GuildMemberUpdateBoostTimeEvent
         SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Guild.class,
                 event -> event.getJDAEvent().getGuild());
 
+        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Member.class,
+                event -> event.getJDAEvent().getMember());
+
+        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, User.class,
+                event -> event.getJDAEvent().getUser());
     }
 
     public static class BukkitMemberBoostEvent extends SimpleDiSkyEvent<GuildMemberUpdateBoostTimeEvent> {
