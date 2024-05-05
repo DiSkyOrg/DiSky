a486cb67bc3d56662015d517e5d1a665e8018e02
Sky <livegogo.16334@gmail.com>
2024-05-05 15:32:19 +0200

    :fire: Needs rework

--- src/main/java/info/itsthesky/disky/elements/events/member/MemberBoostEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/member/MemberBoostEvent.java
@@ -1,45 +1,71 @@
 package info.itsthesky.disky.elements.events.member;
 
-import ch.njol.skript.util.Date;
 import info.itsthesky.disky.api.events.DiSkyEvent;
 import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.api.events.specific.MessageEvent;
 import info.itsthesky.disky.core.SkriptUtils;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-import java.time.OffsetDateTime;
+public class MemberBoostEvent extends DiSkyEvent<MessageReceivedEvent> {
 
-public class MemberBoostEvent extends DiSkyEvent<GuildMemberUpdateBoostTimeEvent> {
-
-    static {
-        register("Member Boost Time Update Event", MemberBoostEvent.class, BukkitMemberBoostEvent.class,
-                "[discord] [guild] member boost time (change|update)");
-
-        SkriptUtils.registerBotValue(MemberBoostEvent.BukkitMemberBoostEvent.class);
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Date.class,
-                event -> SkriptUtils.convertDateTime(event.getJDAEvent().getOldValue()), -1);
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Date.class,
-                event -> SkriptUtils.convertDateTime(event.getJDAEvent().getNewValue()), 0);
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Date.class,
-                event -> SkriptUtils.convertDateTime(event.getJDAEvent().getNewValue()), 1);
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Guild.class,
-                event -> event.getJDAEvent().getGuild());
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, Member.class,
-                event -> event.getJDAEvent().getMember());
-
-        SkriptUtils.registerValue(MemberBoostEvent.BukkitMemberBoostEvent.class, User.class,
-                event -> event.getJDAEvent().getUser());
+    @Override
+    public boolean check(MessageReceivedEvent event) {
+        return event.isFromGuild()
+                && event.getMessage().getType().isSystem()
+                && event.getMessage().getType().equals(MessageType.GUILD_MEMBER_BOOST);
     }
 
-    public static class BukkitMemberBoostEvent extends SimpleDiSkyEvent<GuildMemberUpdateBoostTimeEvent> {
-        public BukkitMemberBoostEvent(MemberBoostEvent event) {
+    static {
+        /*register("Member Boost Event",
+                MemberBanEvent.class, BukkitMemberBoostEvent.class,
+                "[discord] member boost[ed]");*/
+
+        SkriptUtils.registerBotValue(BukkitMemberBoostEvent.class);
+
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, Message.class,
+                event -> event.getJDAEvent().getMessage());
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, Guild.class,
+                event -> event.getJDAEvent().getGuild());
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, User.class,
+                event -> event.getJDAEvent().getAuthor());
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, MessageChannel.class,
+                event -> event.getJDAEvent().getChannel());
+
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, GuildChannel.class,
+                event -> event.getJDAEvent().isFromGuild() ? event.getJDAEvent().getGuildChannel() : null);
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, TextChannel.class,
+                event -> event.getJDAEvent().isFromType(ChannelType.TEXT) ? event.getJDAEvent().getChannel().asTextChannel() : null);
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, NewsChannel.class,
+                event -> event.getJDAEvent().isFromType(ChannelType.NEWS) ? event.getJDAEvent().getChannel().asNewsChannel() : null);
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, ThreadChannel.class,
+                event -> event.getJDAEvent().isFromType(ChannelType.GUILD_PUBLIC_THREAD) || event.getJDAEvent().isFromType(ChannelType.GUILD_PRIVATE_THREAD) ? event.getJDAEvent().getChannel().asThreadChannel() : null);
+
+        SkriptUtils.registerValue(BukkitMemberBoostEvent.class, PrivateChannel.class,
+                event -> !event.getJDAEvent().isFromGuild() ? event.getJDAEvent().getChannel().asPrivateChannel() : null);
+    }
+
+    public static class BukkitMemberBoostEvent
+            extends SimpleDiSkyEvent<MessageReceivedEvent>
+            implements MessageEvent {
+
+        public BukkitMemberBoostEvent(MemberBoostEvent event) { }
+
+        @Override
+        public MessageChannel getMessageChannel() {
+            return getJDAEvent().getChannel();
+        }
+
+        @Override
+        public boolean isFromGuild() {
+            return getJDAEvent().isFromGuild();
         }
     }
 }
