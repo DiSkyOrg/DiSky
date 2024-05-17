b3baecdecfd99498bfd44ebe6b107d61446b0037
Sky <livegogo.16334@gmail.com>
2024-05-17 18:45:02 +0200

    :sparkles: Added past/future event-date for member timeout event

--- src/main/java/info/itsthesky/disky/elements/events/member/MemberTimeoutEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/member/MemberTimeoutEvent.java
@@ -1,13 +1,15 @@
 package info.itsthesky.disky.elements.events.member;
 
+import ch.njol.skript.util.Date;
 import info.itsthesky.disky.api.events.DiSkyEvent;
 import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
 import info.itsthesky.disky.core.SkriptUtils;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateTimeOutEvent;
+
+import static info.itsthesky.disky.core.SkriptUtils.convertDateTime;
 
 public class MemberTimeoutEvent extends DiSkyEvent<GuildMemberUpdateTimeOutEvent> {
 
@@ -27,9 +29,15 @@ public class MemberTimeoutEvent extends DiSkyEvent<GuildMemberUpdateTimeOutEvent
         SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, User.class,
                 event -> event.getJDAEvent().getUser(), 0);
 
+        SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
+                event -> convertDateTime(event.getJDAEvent().getNewTimeOutEnd()), 1);
+        SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
+                event -> convertDateTime(event.getJDAEvent().getOldTimeOutEnd()), -1);
+        SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
+                event -> convertDateTime(event.getJDAEvent().getNewTimeOutEnd()), 0);
     }
 
-    public static class BukkitMemberTimeoutEvent extends SimpleDiSkyEvent<GuildMemberJoinEvent> {
+    public static class BukkitMemberTimeoutEvent extends SimpleDiSkyEvent<GuildMemberUpdateTimeOutEvent> {
         public BukkitMemberTimeoutEvent(MemberTimeoutEvent event) {
         }
     }
