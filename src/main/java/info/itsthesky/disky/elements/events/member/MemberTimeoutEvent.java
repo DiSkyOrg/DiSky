83484ce6077770e0053210beb71259ea61e28db8
Sky <livegogo.16334@gmail.com>
2024-05-17 19:38:40 +0200

    :zap: Fixed/enhanced event-values of the member timeout event

--- src/main/java/info/itsthesky/disky/elements/events/member/MemberTimeoutEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/member/MemberTimeoutEvent.java
@@ -1,13 +1,17 @@
 package info.itsthesky.disky.elements.events.member;
 
+import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.util.Date;
 import info.itsthesky.disky.api.events.DiSkyEvent;
 import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.api.skript.SimpleGetterExpression;
+import info.itsthesky.disky.api.skript.reflects.ReflectEventExpressionFactory;
 import info.itsthesky.disky.core.SkriptUtils;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateTimeOutEvent;
+import org.jetbrains.annotations.NotNull;
 
 import static info.itsthesky.disky.core.SkriptUtils.convertDateTime;
 
@@ -30,15 +34,20 @@ public class MemberTimeoutEvent extends DiSkyEvent<GuildMemberUpdateTimeOutEvent
                 event -> event.getJDAEvent().getUser(), 0);
 
         SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
-                event -> convertDateTime(event.getJDAEvent().getNewTimeOutEnd()), 1);
-        SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
-                event -> convertDateTime(event.getJDAEvent().getOldTimeOutEnd()), -1);
-        SkriptUtils.registerValue(BukkitMemberTimeoutEvent.class, Date.class,
                 event -> convertDateTime(event.getJDAEvent().getNewTimeOutEnd()), 0);
+
+        ReflectEventExpressionFactory.registerEventExpression(
+                "(old|past) date", BukkitMemberTimeoutEvent.class,
+                Date.class, event -> convertDateTime(event.getJDAEvent().getOldTimeOutEnd()));
+
+        ReflectEventExpressionFactory.registerEventExpression(
+                "(future|new) date", BukkitMemberTimeoutEvent.class,
+                Date.class, event -> convertDateTime(event.getJDAEvent().getNewTimeOutEnd()));
     }
 
     public static class BukkitMemberTimeoutEvent extends SimpleDiSkyEvent<GuildMemberUpdateTimeOutEvent> {
         public BukkitMemberTimeoutEvent(MemberTimeoutEvent event) {
+
         }
     }
 }
