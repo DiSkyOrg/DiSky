2e175e51ff6e5c9d569ad00bc709e5b8fdde1cf1
Sky <livegogo.16334@gmail.com>
2024-09-17 10:57:16 +0200

    :zap: ActionProperty now holds the self node

--- src/main/java/info/itsthesky/disky/api/skript/action/ActionProperty.java
+++ src/main/java/info/itsthesky/disky/api/skript/action/ActionProperty.java
@@ -1,12 +1,16 @@
 package info.itsthesky.disky.api.skript.action;
 
 import ch.njol.skript.classes.Changer;
+import ch.njol.skript.config.Node;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 import info.itsthesky.disky.api.changers.ChangeablePropertyExpression;
 import info.itsthesky.disky.api.skript.EasyElement;
+import info.itsthesky.disky.api.skript.INodeHolder;
 import info.itsthesky.disky.core.Bot;
+import info.itsthesky.disky.elements.changers.IAsyncChangeableExpression;
+import info.itsthesky.disky.elements.changers.IAsyncGettableExpression;
 import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
@@ -14,8 +18,12 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 
-public abstract class ActionProperty<E, T extends AuditableRestAction, O> extends ChangeablePropertyExpression<Object, O> {
+@SuppressWarnings("unchecked")
+public abstract class ActionProperty<E, T extends AuditableRestAction, O>
+        extends ChangeablePropertyExpression<Object, O>
+        implements IAsyncChangeableExpression, IAsyncGettableExpression<O>, INodeHolder {
 
+    protected Node node;
     public void updateEntity(T newAction, Event event) {
         getExpr().change(event, newAction == null ? new Object[0] : new Object[] {newAction}, Changer.ChangeMode.SET);
     }
@@ -26,33 +34,47 @@ public abstract class ActionProperty<E, T extends AuditableRestAction, O> extend
             return;
         final O value = (O) delta[0];
         final Object entity = EasyElement.parseSingle(getExpr(), e, null);
-        /*try {
-            change((E) entity, value);
-        } catch (ClassCastException ex) {
-            updateEntity(change((T) entity, value), e);
-        }*/
-        // Check if entity is E or T
         if (entity instanceof AuditableRestAction) {
             updateEntity(change((T) entity, value), e);
         } else {
-            change((E) entity, value);
+            change((E) entity, value, false);
         }
     }
 
-    public abstract void change(E role, O value);
+    @Override
+    public void changeAsync(Event e, Object[] delta, Changer.ChangeMode mode) {
+        if (!EasyElement.isValid(delta))
+            return;
+        final O value = (O) delta[0];
+        final Object entity = EasyElement.parseSingle(getExpr(), e, null);
+        if (entity instanceof AuditableRestAction) {
+            ((AuditableRestAction<?>) entity).queue();
+        } else {
+            change((E) entity, value, true);
+        }
+    }
+
+    public abstract void change(E role, O value, boolean async);
 
     public abstract T change(T action, O value);
 
-    public abstract O get(E role);
+    public abstract O get(E role, boolean async);
 
     @Override
     protected O @NotNull [] get(@NotNull Event e, Object @NotNull [] source) {
-        return (O[]) Arrays.stream(source).map(object -> get((E) object)).toArray(Object[]::new);
+        return (O[]) Arrays.stream(source).map(object -> get((E) object, false)).toArray(Object[]::new);
+    }
+
+    @Override
+    public O[] getAsync(Event e) {
+        final Expression<?> expr = getExpr();
+        return (O[]) Arrays.stream(expr.getArray(e)).map(object -> get((E) object, true)).toArray(Object[]::new);
     }
 
     @Override
     public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
         setExpr(exprs[0]);
+        node = getParser().getNode();
         return true;
     }
 
@@ -65,5 +87,10 @@ public abstract class ActionProperty<E, T extends AuditableRestAction, O> extend
         return (Class<T>) ((ParameterizedType) getClass()
                 .getGenericSuperclass()).getActualTypeArguments()[1];
     }
-    
+
+    @Override
+    @NotNull
+    public Node getNode() {
+        return node;
+    }
 }
