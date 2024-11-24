80858da77eb3edf035e2cb5f24f8f6bace439f4c
Sky <livegogo.16334@gmail.com>
2024-11-24 01:47:05 +0100

    :zap: Enhanced open modal effect

--- src/main/java/info/itsthesky/disky/elements/components/OpenModal.java
+++ src/main/java/info/itsthesky/disky/elements/components/OpenModal.java
@@ -1,23 +1,22 @@
 package info.itsthesky.disky.elements.components;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.config.Node;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.util.AsyncEffect;
 import ch.njol.util.Kleenean;
-import info.itsthesky.disky.DiSky;
-import info.itsthesky.disky.api.events.specific.InteractionEvent;
-import info.itsthesky.disky.api.events.specific.MessageEvent;
 import info.itsthesky.disky.api.events.specific.ModalEvent;
-import info.itsthesky.disky.api.skript.WaiterEffect;
-import info.itsthesky.disky.core.Utils;
-import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import info.itsthesky.disky.elements.sections.handler.DiSkyRuntimeHandler;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class OpenModal extends WaiterEffect {
+import static info.itsthesky.disky.api.skript.EasyElement.containsInterfaces;
+import static info.itsthesky.disky.api.skript.EasyElement.parseSingle;
+
+public class OpenModal extends AsyncEffect {
 
 	static {
 		Skript.registerEffect(
@@ -27,30 +26,32 @@ public class OpenModal extends WaiterEffect {
 	}
 
 	private Expression<Modal.Builder> exprModal;
+	private Node node;
 
 	@Override
-	public boolean initEffect(Expression[] expressions, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+	public boolean init(Expression @NotNull [] expressions, int i, @NotNull Kleenean kleenean, SkriptParser.@NotNull ParseResult parseResult) {
 		if (!containsInterfaces(ModalEvent.class)) {
 			Skript.error("The show modal effect can only be used in interactions outside of modal events.");
 			return false;
 		}
+		node = getParser().getNode();
 		exprModal = (Expression<Modal.Builder>) expressions[0];
 		return true;
 	}
 
 	@Override
-	public void runEffect(Event e) {
-		final Modal.Builder modal = parseSingle(exprModal, e, null);
-		if (modal == null) {
-			restart();
+	protected void execute(@NotNull Event event) {
+		final Modal.Builder modal = parseSingle(exprModal, event, null);
+		if (modal == null)
+			return;
+
+		if (modal.getComponents().isEmpty() || modal.getComponents().size() > 5) {
+			DiSkyRuntimeHandler.error(new IllegalArgumentException("A modal must have between 1 and 5 components!"), node);
 			return;
 		}
-		Utils.catchAction(((ModalEvent) e).replyModal(modal.build()), v -> {
-			restart();
-		}, ex -> {
-			restart();
-			DiSky.getErrorHandler().exception(e, ex);
-		});
+
+		final ModalEvent modalEvent = (ModalEvent) event;
+		modalEvent.replyModal(modal.build()).complete();
 	}
 
 	@Override
