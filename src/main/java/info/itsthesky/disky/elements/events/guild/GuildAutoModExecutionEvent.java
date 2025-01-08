b5ff9f4afe6640e4c1c04b2f856794515f4a9269
Sky <livegogo.16334@gmail.com>
2025-01-08 09:21:20 +0100

    :zap: Added "latest message id"'s event property

--- src/main/java/info/itsthesky/disky/elements/events/guild/GuildAutoModExecutionEvent.java
+++ src/main/java/info/itsthesky/disky/elements/events/guild/GuildAutoModExecutionEvent.java
@@ -1,12 +1,17 @@
 package info.itsthesky.disky.elements.events.guild;
 
+import ch.njol.skript.util.Date;
 import info.itsthesky.disky.api.events.DiSkyEvent;
 import info.itsthesky.disky.api.events.SimpleDiSkyEvent;
+import info.itsthesky.disky.api.skript.reflects.ReflectEventExpressionFactory;
 import info.itsthesky.disky.core.SkriptUtils;
+import info.itsthesky.disky.elements.events.member.MemberTimeoutEvent;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.automod.AutoModResponse;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.unions.GuildMessageChannelUnion;
+
+import static info.itsthesky.disky.core.SkriptUtils.convertDateTime;
 
 public class GuildAutoModExecutionEvent extends DiSkyEvent<net.dv8tion.jda.api.events.automod.AutoModExecutionEvent> {
 
@@ -15,7 +20,6 @@ public class GuildAutoModExecutionEvent extends DiSkyEvent<net.dv8tion.jda.api.e
                 "[discord] automod (execution|execute)")
                 .description("Fired when an automated automod response has been triggered through an automod Rule. Can be used to get the channel, user content, keyword that was found, the automod response and the id of the automod rule, the user, the id of the message which triggered the rule, the guild it occurred in, and the id of the alert message sent to the alert channel (if configured).")
                 .examples("on automod execute:");
-
 
         SkriptUtils.registerBotValue(GuildAutoModExecutionEvent.BukkitAutoModExecutionEvent.class);
 
@@ -28,6 +32,9 @@ public class GuildAutoModExecutionEvent extends DiSkyEvent<net.dv8tion.jda.api.e
         SkriptUtils.registerValue(GuildAutoModExecutionEvent.BukkitAutoModExecutionEvent.class, AutoModResponse.class,
                 event -> event.getJDAEvent().getResponse(), 0);
 
+        ReflectEventExpressionFactory.registerEventExpression(
+                "alert message id", GuildAutoModExecutionEvent.BukkitAutoModExecutionEvent.class,
+                String.class, event -> event.getJDAEvent().getAlertMessageId());
     }
 
     public static class BukkitAutoModExecutionEvent extends SimpleDiSkyEvent<net.dv8tion.jda.api.events.automod.AutoModExecutionEvent> {
