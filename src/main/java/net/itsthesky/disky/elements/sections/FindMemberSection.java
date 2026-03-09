729ffb2a3259c9d22289d64c337c10f1163a6b62
ItsTheSky <livegogo.16334@gmail.com>
2026-03-09 09:42:45 +0100

    :sparkles: Fixed multi-threading and variables de-sync with the find member section

--- src/main/java/net/itsthesky/disky/elements/sections/FindMemberSection.java
+++ src/main/java/net/itsthesky/disky/elements/sections/FindMemberSection.java
@@ -28,7 +28,7 @@ import java.util.List;
 
 @Name("Find Members")
 @Description({"Find member filter! It's a section that lets you filter by yourself the members you want to retrieve.", "Wants only members with a role, or specific nickname, that are not connected to a channel? You've got it!"})
-@Examples({"find members in event-guild and store them in {_members::*} with filter var {_m}:", "\t# {_m} now contains the member to apply the filter to. For instance:", "\t{_m} has discord role with id \"XXX\"", "\t{_m} is muted", "\treturn true", "reply with \"I have found %size of {_members::*}% that has the role and is muted!\""})
+@Examples({"find members in event-guild and store them in {_members::*} with filter var {_m}:", "\t# {_m} now contains the member to apply the filter to. For instance:", "\tif {_m} has discord role with id \"XXX\":", "\t\treturn true", "reply with \"I have found %size of {_members::*}% that has the role!\""})
 @Since("4.14.3")
 @SeeAlso({Member.class, Guild.class})
 public class FindMemberSection extends Section implements ReturnHandler<Boolean> {
@@ -42,25 +42,22 @@ public class FindMemberSection extends Section implements ReturnHandler<Boolean>
     private Expression<Object> exprValue;
 
     private ReturnableTrigger<Boolean> trigger;
+
     private boolean iterationResult = false;
 
     @Override
-    public boolean init(Expression<?>[] expressions, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult, @NotNull SectionNode sectionNode, @NotNull List<TriggerItem> triggerItems) {
+    public boolean init(Expression<?>[] expressions, int matchedPattern, @NotNull Kleenean isDelayed,
+                        @NotNull ParseResult parseResult, @NotNull SectionNode sectionNode,
+                        @NotNull List<TriggerItem> triggerItems) {
         exprGuild = (Expression<Guild>) expressions[0];
         exprResult = (Expression<Object>) expressions[1];
         exprValue = (Expression<Object>) expressions[2];
 
-        if (!Changer.ChangerUtils.acceptsChange(exprValue, Changer.ChangeMode.SET, Member.class) || !Changer.ChangerUtils.acceptsChange(exprResult, Changer.ChangeMode.SET, Member[].class))
+        if (!Changer.ChangerUtils.acceptsChange(exprValue, Changer.ChangeMode.SET, Member.class)
+                || !Changer.ChangerUtils.acceptsChange(exprResult, Changer.ChangeMode.SET, Member[].class))
             return false;
 
         trigger = loadReturnableSectionCode(sectionNode, "find members", getParser().getCurrentEvents());
-
-        // TODO: Be sure we don't have any delay within the section
-//        if (delayed.get()) {
-//            Skript.error("Delays can't be used within a 'find member' section.");
-//            return false;
-//        }
-
         return true;
     }
 
@@ -88,38 +85,42 @@ public class FindMemberSection extends Section implements ReturnHandler<Boolean>
         if (guild == null) return getNext();
 
         debug(e, true);
-
         Delay.addDelayedEvent(e);
+
         Object localVars = Variables.removeLocals(e);
 
         Bukkit.getScheduler().runTaskAsynchronously(DiSky.getInstance(), () -> {
-            if (localVars != null) Variables.setLocalVariables(e, localVars);
+
+            if (localVars != null)
+                Variables.setLocalVariables(e, localVars);
 
             try {
                 List<Member> members = guild.findMembers(member -> {
                     iterationResult = false;
                     exprValue.change(e, new Member[]{member}, Changer.ChangeMode.SET);
-                    trigger.execute(e);
+
+                    TriggerItem.walk(trigger, e);
                     return iterationResult;
                 }).get();
 
                 exprResult.change(e, members.toArray(new Member[0]), Changer.ChangeMode.SET);
+
             } catch (Exception ex) {
-                DiSkyRuntimeHandler.error((Exception) ex);
+                DiSkyRuntimeHandler.error(ex);
             }
 
-            // Restarts the following code
             if (getNext() != null) {
                 Bukkit.getScheduler().runTask(Skript.getInstance(), () -> {
                     Object timing = null;
                     if (SkriptTimings.enabled()) {
-                        Trigger trigger = getTrigger();
-                        if (trigger != null) {
-                            timing = SkriptTimings.start(trigger.getDebugLabel());
+                        Trigger t = getTrigger();
+                        if (t != null) {
+                            timing = SkriptTimings.start(t.getDebugLabel());
                         }
                     }
 
                     TriggerItem.walk(getNext(), e);
+
                     Variables.removeLocals(e);
                     SkriptTimings.stop(timing);
                 });
@@ -133,6 +134,8 @@ public class FindMemberSection extends Section implements ReturnHandler<Boolean>
 
     @Override
     public @NotNull String toString(@Nullable Event event, boolean debug) {
-        return "find members in guild " + exprGuild.toString(event, debug) + " and store them in " + exprResult.toString(event, debug) + " with filter " + exprValue.toString(event, debug);
+        return "find members in guild " + exprGuild.toString(event, debug)
+                + " and store them in " + exprResult.toString(event, debug)
+                + " with filter " + exprValue.toString(event, debug);
     }
 }
