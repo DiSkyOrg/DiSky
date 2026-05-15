60eb754e97b0a8d667a3057aa7bdf984bfb6f411
P0wders <139374380+P0wders@users.noreply.github.com>
2026-05-15 13:19:08 +0200

    🚩 Add getTimeCreated() to retrieve invite creation date (#315)
    * 🚩Add getTimeCreated() to retrieve the skript date of when was the invite created at.
    
    * ♻️ Refactor invite creation date conversion to use `SkriptUtils.convertDateTime`.
    
    ---------
    
    Co-authored-by: P0wders <139374380+PROxxHC@users.noreply.github.com>

---
+++ src/main/java/net/itsthesky/disky/elements/properties/invites/InviteCreationDate.java
@@ -0,0 +1,40 @@
+package net.itsthesky.disky.elements.properties.invites;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.util.Date;
+import net.dv8tion.jda.api.entities.Invite;
+import net.itsthesky.disky.api.generator.SeeAlso;
+import net.itsthesky.disky.core.SkriptUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Invite Creation Date")
+@Description({"Returns the creation date of the given invite.",
+        "This only works on expanded invites and will throw an IllegalStateException otherwise."})
+@Examples({"set {_created} to invite creation date of event-invite",
+        "reply with invite creation date of event-invite"})
+@Since("4.29.0")
+@SeeAlso(Invite.class)
+public class InviteCreationDate extends InviteProperty<Date> {
+
+    static {
+        register(
+                InviteCreationDate.class,
+                Date.class,
+                "creation date"
+        );
+    }
+
+    @Override
+    public @NotNull Class<? extends Date> getReturnType() {
+        return Date.class;
+    }
+
+    @Override
+    public @Nullable Date convert(Invite invite) {
+        return SkriptUtils.convertDateTime(invite.getTimeCreated());
+    }
+}
