fabec5087a907ef3ebed2468768d52275b0450fd
P0wders <139374380+PROxxHC@users.noreply.github.com>
2026-05-15 12:09:15 +0200

    ♻️ Refactor invite creation date conversion to use `SkriptUtils.convertDateTime`.

--- src/main/java/net/itsthesky/disky/elements/properties/invites/InviteCreationDate.java
+++ src/main/java/net/itsthesky/disky/elements/properties/invites/InviteCreationDate.java
@@ -7,17 +7,16 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.util.Date;
 import net.dv8tion.jda.api.entities.Invite;
 import net.itsthesky.disky.api.generator.SeeAlso;
+import net.itsthesky.disky.core.SkriptUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.TimeZone;
 
 @Name("Invite Creation Date")
 @Description({"Returns the creation date of the given invite.",
         "This only works on expanded invites and will throw an IllegalStateException otherwise."})
 @Examples({"set {_created} to invite creation date of event-invite",
         "reply with invite creation date of event-invite"})
-@Since("4.28.0")
+@Since("4.29.0")
 @SeeAlso(Invite.class)
 public class InviteCreationDate extends InviteProperty<Date> {
 
@@ -36,6 +35,6 @@ public class InviteCreationDate extends InviteProperty<Date> {
 
     @Override
     public @Nullable Date convert(Invite invite) {
-        return new Date(invite.getTimeCreated().toInstant().toEpochMilli(), TimeZone.getTimeZone("GMT"));
+        return SkriptUtils.convertDateTime(invite.getTimeCreated());
     }
 }
